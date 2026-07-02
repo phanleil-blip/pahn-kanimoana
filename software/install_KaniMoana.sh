c0e3e9310685d0c56fdadccae58d746a9f5bee6b
phanleil-blip <phanleil@hawaii.edu>
2026-07-01 17:02:29 -1000

    Improve readability of .bashrc entries
    Added empty lines for better readability in .bashrc.

--- software/install_KaniMoana.sh
+++ software/install_KaniMoana.sh
@@ -146,7 +146,9 @@ sudo echo "sleep 10" >> .bashrc
 sudo echo "" >> .bashrc
 sudo echo "cd /home/pi/kanimoana" >> .bashrc
 sudo echo "sudo ./KaniMoana.sh" >> .bashrc
+sudo echo "" >> .bashrc
 sudo echo "# wittyPi Scheduling and Shutdown Sequence for KaniMoana" >> .bashrc
+sudo echo "" >> .bashrc
 sudo echo "sudo ./shutdown_now.sh"  >> .bashrc
 
 # ------------------------------------------------------------
